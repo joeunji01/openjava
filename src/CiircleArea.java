5ceaed7c76b97edaed8f2be6ca3f17c71e222c96
joeunji01 <eunzs@naver.com>
2022-09-15 23:42:42 +0900

    commit1

---
+++ src/CiircleArea.java
@@ -0,0 +1,14 @@
+
+public class CiircleArea {
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI =3.14; //원주율을 상수로 선언 
+		
+		double radius=10.0;
+		double circleArea =radius*radius*PI; //원의 면적 
+		
+		System.out.println("원의 면적= "+circleArea); //연결 연산 
+	}
+
+}
